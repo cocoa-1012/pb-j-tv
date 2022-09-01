f6f394a5b09c52859cc6a57c627179e1e8b834aa
Monirul Islam <Monirul Islam>
2022-09-01 22:20:01 +0600

    photo slide page button toggle

--- src/components/PhotoSlide/Top.js
+++ src/components/PhotoSlide/Top.js
@@ -1,9 +1,16 @@
 import { Button, Grid, Stack, Typography } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 const Top = () => {
+  const [slideShowPosition, setSlideShowPosition] = useState('');
   const toggle = (value) => {
+    setSlideShowPosition(value);
     localStorage.setItem('slideShowPosition', value);
   };
+
+  useEffect(() => {
+    setSlideShowPosition(localStorage.getItem('slideShowPosition') || '');
+  }, []);
+
   return (
     <div>
       <Grid container sx={{ borderBottom: '1px solid green', mb: '10px' }}>
@@ -14,12 +21,16 @@ const Top = () => {
         </Grid>
         <Grid xs={12} md={3}>
           <Stack direction='row' spacing={2} sx={{ ml: '10px' }}>
-            <Button variant='contained' onClick={() => toggle('top')}>
+            <Button
+              variant='contained'
+              color={slideShowPosition === 'top' ? 'primary' : 'secondary'}
+              onClick={() => toggle('top')}
+            >
               Top
             </Button>
             <Button
               variant='contained'
-              color='secondary'
+              color={slideShowPosition === 'bottom' ? 'primary' : 'secondary'}
               onClick={() => toggle('bottom')}
             >
               Bottom
